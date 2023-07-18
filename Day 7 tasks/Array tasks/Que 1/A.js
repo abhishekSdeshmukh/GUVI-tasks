713c2137ff61dab7cb297e708d21149d9b44e24d
abhishekSdeshmukh <125177074+abhishekSdeshmukh@users.noreply.github.com>
2023-07-18 15:54:18 +0530

    Delete Day 7 tasks directory

--- Day 7 tasks/Array tasks/Que 1/A.js
+++
@@ -1,9 +0,0 @@
-// Get all the countries from the Asia continent /region using the Filter function
-fetch('https://restcountries.com/v3.1/all')
-  .then(response => response.json())
-  .then(data => {
-    const asiaCountries = data.filter(country => country.region === 'Asia');
-    const countryNamesInAsia = asiaCountries.map(country => country.name.common);
-    console.log(countryNamesInAsia);
-  })
-  .catch(error => console.error('Error fetching data:', error));
